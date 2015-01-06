aa9a1daee9c39e7b73d81e6e76df0b6d10fc33c1
NickThissen <nick.thissen@gmail.com>
2015-01-06 18:26:38 +0100

    Revert "Bump version"
    This reverts commit f8e947770293fd71f37b83cd824a141e90f64420.

--- iRTVO/Properties/AssemblyInfo.cs
+++ iRTVO/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("iRacing TV Overlay 1.3.0-RC10")]
+[assembly: AssemblyTitle("iRacing TV Overlay 1.3.0-RC9")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
